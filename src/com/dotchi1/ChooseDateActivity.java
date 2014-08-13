b86acea37d0dd1ce25738f484f6c401758cab5f9
Ivan <ivanpeng13@gmail.com>
2014-08-13 15:21:05 +0800

    Date changes.

--- src/com/dotchi1/ChooseDateActivity.java
+++ src/com/dotchi1/ChooseDateActivity.java
@@ -1,19 +1,30 @@
 package com.dotchi1;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.app.AlertDialog.Builder;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.squareup.timessquare.CalendarPickerView;
 import com.squareup.timessquare.CalendarPickerView.OnDateSelectedListener;
@@ -24,6 +35,9 @@ public class ChooseDateActivity extends Activity {
 	private ListView dateListView;
 	private ArrayList<Date> dates = new ArrayList<Date>();
 	private ArrayAdapter<Date> adapter;
+	
+	private int h = 0, m = 0;
+	private Date d;
 	
 
 	@Override
@@ -68,25 +82,108 @@ public class ChooseDateActivity extends Activity {
 		});
 	}
 	
+	protected void chooseTimeDialog(final Date date)	{
+		d = null;
+		final Calendar cal = Calendar.getInstance();
+		
+		View timePickerView = getLayoutInflater().inflate(R.layout.time_picker_view, null);
+
+		// This function opens up a dialog and creates a date
+		AlertDialog.Builder builder = new AlertDialog.Builder(this)
+			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					Log.d("Calendar", "Entered Here!");
+					h = ;
+					m = ;
+					cal.setTime(date);
+					cal.set(Calendar.HOUR, h);
+					cal.set(Calendar.MINUTE, m);
+					d = cal.getTime();
+					dates.add(d);
+					adapter.notifyDataSetChanged();
+				}
+			})
+			.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			})
+			.setView(timePickerView);
+
+	}
+	
 	class CalendarAdapter extends ArrayAdapter<Date>	{
 
 		private Context context;
 		private ArrayList<Date> objects;
+		private SimpleDateFormat dateFormat, dayOfWeekFormat, timeFormat;
 		
 		public CalendarAdapter(Context context, ArrayList<Date> objects) {
 			super(context, 0, objects);
 			this.context = context;
 			this.objects = objects;
+			dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			dayOfWeekFormat = new SimpleDateFormat("E");
+			timeFormat = new SimpleDateFormat("hh:mm a");
+		}
+
+		@Override
+		public void add(Date object) {
+			//objects.add(object);
+			Log.d("Calendar", "Added date " +object.toString());
+			super.add(object);
+		}
+
+		@Override
+		public void remove(Date object) {
+			//objects.remove(object);
+			super.remove(object);
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			View view = convertView;
-			if (view == null)	{
-				LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				//view = inflater.inflate(R.layout.);
+			final Date item = objects.get(position);
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(item);
+			View view = null;
+			// Don't recycle views
+			LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			if (cal.get(Calendar.HOUR) == 0 && cal.get(Calendar.MINUTE) == 0)	{
+				view = inflater.inflate(R.layout.date_text_item, null);
+			
+				TextView dateView = (TextView) view.findViewById(R.id.date_headline);
+				dateView.setText(dateFormat.format(item));
+				TextView dayOfWeekView = (TextView) view.findViewById(R.id.day_of_week);
+				dayOfWeekView.setText(dayOfWeekFormat.format(item));
+				// Add adapter for clicking add date
+				Button addDateButton = (Button) view.findViewById(R.id.add_date_button);
+				addDateButton.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						// Pop up alert dialog and set date.
+						chooseTimeDialog(item);
+					}
+	
+	
+				});
+			} else	{
+				view = inflater.inflate(R.layout.date_text_time_item, null);
+				TextView timeView = (TextView) view.findViewById(R.id.time_header);
+				timeView.setText(timeFormat.format(item));
+				// Set delete button
+				ImageView deleteButton = (ImageView) view.findViewById(R.id.delete_time);
+				deleteButton.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						remove(item);
+					}
+				});
 			}
-			return super.getView(position, convertView, parent);
+			return view;
 		}
 
 		@Override
