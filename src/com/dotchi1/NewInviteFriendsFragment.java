6c3df1c4876437814b2623cb1e6b428e0b807943
Ivan <ivanpeng13@gmail.com>
2014-09-11 19:19:44 +0800

    Forgot to commit.

---
+++ src/com/dotchi1/NewInviteFriendsFragment.java
@@ -0,0 +1,24 @@
+package com.dotchi1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewInviteFriendsFragment extends Fragment {
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.new_invite_friends_fragment, null);
+		return view;
+	}
+
+}
